98ad77206dc90fc99c5174eeec1753f6fdee0743
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-08-31 02:53:19 +0200

    Bump Daily Flare Reel to v24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thedailyflare.reel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "23"
+        versionCode = 24
+        versionName = "24"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
