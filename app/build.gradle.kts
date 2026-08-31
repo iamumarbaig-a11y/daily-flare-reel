cc971a0b088f130c799cb64280af0c80dbe9074d
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-08-31 03:20:31 +0200

    Bump reel app to v31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thedailyflare.reel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "29"
+        versionCode = 31
+        versionName = "31"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
