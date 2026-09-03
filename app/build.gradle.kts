05db37adb4271b57d26d101fd578301a3e690354
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-09-03 23:58:30 +0200

    Add DocumentFile support for Kokoro package import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation("androidx.media3:media3-transformer:1.10.1")
     implementation("androidx.media3:media3-common:1.10.1")
+    implementation("androidx.documentfile:documentfile:1.0.1")
     implementation(files("libs/sherpa-onnx-1.13.7.aar"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
