1f55cbfdc0eff36e03f0c0aee6df7ef4c0d8e71e
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-09-07 23:55:43 +0200

    Add EGL codec input surface with deterministic timestamps

---
+++ app/src/main/java/com/thedailyflare/reel/CodecInputSurface.kt
@@ -0,0 +1,34 @@
+package com.thedailyflare.reel
+
+import android.opengl.EGL14
+import android.opengl.EGLExt
+import android.view.Surface
+
+/** EGL wrapper for MediaCodec input surfaces with explicit frame timestamps. */
+class CodecInputSurface(private val surface: Surface) {
+    private val display: android.opengl.EGLDisplay
+    private val context: android.opengl.EGLContext
+    private val eglSurface: android.opengl.EGLSurface
+
+    init {
+        display = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY)
+        check(display != EGL14.EGL_NO_DISPLAY) { "Unable to get EGL display" }
+        val version = IntArray(2)
+        check(EGL14.eglInitialize(display, version, 0, version, 1)) { "Unable to initialize EGL" }
+        val attribs = intArrayOf(EGL14.EGL_RED_SIZE,8,EGL14.EGL_GREEN_SIZE,8,EGL14.EGL_BLUE_SIZE,8,EGL14.EGL_ALPHA_SIZE,8,0x3142,1,EGL14.EGL_NONE)
+        val configs = arrayOfNulls<android.opengl.EGLConfig>(1); val num = IntArray(1)
+        check(EGL14.eglChooseConfig(display, attribs,0,configs,0,1,num,0) && num[0]>0) { "No EGL config" }
+        context = EGL14.eglCreateContext(display,configs[0],EGL14.EGL_NO_CONTEXT,intArrayOf(EGL14.EGL_CONTEXT_CLIENT_VERSION,2,EGL14.EGL_NONE),0)
+        check(context != EGL14.EGL_NO_CONTEXT) { "Unable to create EGL context" }
+        eglSurface = EGL14.eglCreateWindowSurface(display,configs[0],surface,intArrayOf(EGL14.EGL_NONE),0)
+        check(eglSurface != EGL14.EGL_NO_SURFACE) { "Unable to create EGL window surface" }
+    }
+    fun makeCurrent() { check(EGL14.eglMakeCurrent(display,eglSurface,eglSurface,context)) { "Unable to make EGL current" } }
+    fun setPresentationTime(nsecs: Long) { EGLExt.eglPresentationTimeANDROID(display,eglSurface,nsecs) }
+    fun swapBuffers() { check(EGL14.eglSwapBuffers(display,eglSurface)) { "eglSwapBuffers failed" } }
+    fun release() {
+        EGL14.eglMakeCurrent(display,EGL14.EGL_NO_SURFACE,EGL14.EGL_NO_SURFACE,EGL14.EGL_NO_CONTEXT)
+        EGL14.eglDestroySurface(display,eglSurface); EGL14.eglDestroyContext(display,context)
+        EGL14.eglReleaseThread(); EGL14.eglTerminate(display); surface.release()
+    }
+}
