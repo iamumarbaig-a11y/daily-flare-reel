ad07c075e9c8d869981e25753bbf6ae27f39463a
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-09-07 21:17:51 +0200

    Fix missing View import in Kokoro package screen

--- app/src/main/java/com/thedailyflare/reel/KokoroExperimentActivity.kt
+++ app/src/main/java/com/thedailyflare/reel/KokoroExperimentActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import android.graphics.drawable.GradientDrawable
