5d88c1ed83b0d114b09e88aade3eb781b2012c77
iamumarbaig-a11y <iamumarbaig@gmail.com>
2026-09-11 20:00:06 +0200

    Restore required media dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 dependencies {
     implementation("androidx.exifinterface:exifinterface:1.3.7")
+    implementation("androidx.media3:media3-transformer:1.10.1")
+    implementation("androidx.media3:media3-common:1.10.1")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("dev.ffmpegkit-maintained:ffmpeg-kit-video:8.1.7")
     implementation(files("libs/sherpa-onnx-1.13.7.aar"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
